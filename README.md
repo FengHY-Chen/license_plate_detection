a3ca7ad36b42b9aa5dbcaee943fa533c1c7d27ae
Sindhu Harish <sindu.hrsh@gmail.com>
2020-11-19 10:14:47 +0000

    license_plate_detection

---
+++ README.md
@@ -0,0 +1,32 @@
+# License Plate Hide
+
+### Pipeline Overview
+
+To build and test your object detection algorithm follow the below steps:
+
+ 1. [Image Annotation](/1_Image_Annotation/)
+	 - Microsoft's Visual Object Tagging Tool (VoTT)
+	 - Annotate images
+ 2. [Training](/2_Training/)
+ 	- Download pre-trained weights Yolo
+ 	- Train model to detect license plates
+ 3. [Inference](/3_Inference/)
+ 	- Run Inference 
+
+### Unizip files
+
+  1. Unzip src and source_images in respective folders(Training and Data folders)
+  2. Run Detector in inference folder
+  3. Results are stored in Data Folder
+
+
+### To Train
+  1. Download the Microsoft's Visual Object Tagging Tool (VoTT)
+  2. Annotate images
+  3. Convert to yolo format
+  4. Run Train_yolo.py file
+
+### Inference
+
+![27_license_plate](https://user-images.githubusercontent.com/46603188/79003470-aecc0e80-7b20-11ea-998b-12c123d3540b.jpg)
+![97_license_plate](https://user-images.githubusercontent.com/46603188/79003477-b25f9580-7b20-11ea-9762-85ee8489b80a.jpg)
---
+++ Utils/Get_File_Paths.py
@@ -0,0 +1,54 @@
+from os import path, makedirs
+import os
+def GetFileList(dirName,endings=['.jpg','.jpeg','.png','.mp4']):
+
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+
+
+    for i,ending in enumerate(endings):
+        if ending[0]!='.':
+            endings[i] = '.'+ending
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory 
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + GetFileList(fullPath,endings)
+        else:
+            for ending in endings:
+                if entry.endswith(ending):
+                    allFiles.append(fullPath)               
+    return allFiles        
+ 
+def ChangeToOtherMachine(filelist,repo='TrainYourOwnYOLO',remote_machine =''):
+    '''
+    Takes a list of file_names located in a repo and changes it to the local machines file names. File must be executed from withing the repository
+
+    Example:
+
+    '/home/ubuntu/TrainYourOwnYOLO/Data/Street_View_Images/vulnerable/test.jpg'
+
+    Get's converted to
+    
+    'C:/Users/Ananth/TrainYourOwnYOLO/Data/Street_View_Images/vulnerable/test.jpg'
+
+    for working with windows
+
+    '''
+    filelist = [x.replace("\\","/") for x in filelist]
+    if repo[-1]=='/':
+    	repo=repo[:-1]
+    if remote_machine:
+        prefix = remote_machine.replace("\\","/")
+    else:
+        prefix = ((os.path.dirname(os.path.abspath(__file__)).split(repo))[0]).replace("\\","/")
+    new_list = []
+
+    for file in filelist:
+        suffix = (file.split(repo))[1]
+        if suffix[0]=='/':
+        	suffix = suffix[1:]
+        new_list.append(os.path.join(prefix,repo+'/',suffix).replace("\\","/"))
+    return new_list
